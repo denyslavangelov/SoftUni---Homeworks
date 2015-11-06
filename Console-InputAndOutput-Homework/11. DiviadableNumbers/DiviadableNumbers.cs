6e1d7f69480df6b0d860deed3c4bde67459c3749
denyslavangelov <denyslavangelov@abv.bg>
2015-11-06 22:26:52 +0200

    Updating SoftUni - Homeworks

---
+++ Console-InputAndOutput-Homework/11. DiviadableNumbers/DiviadableNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class DiviadableNumbers
+{
+    static void Main()
+    {
+        // Input.
+        Console.Write("Enter your first number : ");
+        int startingNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter your second number : ");
+        int endingNumber = int.Parse(Console.ReadLine());
+
+        //Spaces.
+        Console.WriteLine();
+
+        // Logic.
+        int counter = 0;
+        for (int i = startingNumber; i <= endingNumber; i++)
+        {
+            if (i % 5 == 0)
+            {
+                Console.WriteLine("Number devided by 5 : {0}", i);
+                counter++;
+            }
+        }
+
+
+        // Spaces.
+        Console.WriteLine();
+
+
+        // Output
+        Console.WriteLine("There are {0} numbers dividable by 5 with reminder 0 in the interval {1} and {2}.", counter, startingNumber, endingNumber);
+
+    }
+}
+
---
+++ Console-InputAndOutput-Homework/14. NewHouse/NewHouse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class NewHouse
+{
+    static void Main()
+    {
+        // Pretty easy.
+
+        // Input.
+        int n = int.Parse(Console.ReadLine());
+
+
+        // Roof.
+        for (int i = 0; i < (n / 2) + 1; i++)
+        {
+            Console.WriteLine("{0}{1}{0}", new string('-', n / 2 - i), new string('*', 1 + 2*i));
+        }
+
+        // Main house.
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("|{0}|", new string('*', n - 2));
+        }
+    }
+}
+
